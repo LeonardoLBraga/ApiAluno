ee1caf57ffd30b8f2b5e53c242f7f7502e81d42e
LeonardoLBraga <Leonardobraga24@gmail.com>
2025-02-13 16:54:19 -0300

    Organizando o código

--- src/ApiAluno/Data/AppDbContext.cs
+++ src/ApiAluno/Data/AppDbContext.cs
@@ -5,8 +5,10 @@ namespace ApiAluno.Data
 {
     public class AppDbContext : DbContext
     {
-        
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options) 
+            { 
+            }
 
         public DbSet<Aluno> Alunos { get; set; }
     }
